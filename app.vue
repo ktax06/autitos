aff38f2ed09abaaefc96a89ac6e1023d88a1fd84
ArturoAlvarezz <arturoalvarezz@gmail.com>
2025-04-23 21:10:32 -0400

    Add Tailwind CSS import to main stylesheet

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline text-yellow-500">
+      Hello world!
+    </h1>
   </div>
 </template>
