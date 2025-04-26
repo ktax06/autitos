840334ff3c5fe71fa48cbd7f1f29537935d03d02
ktax06 <prettycaty7@gmail.com>
2025-04-26 19:37:26 -0400

    interfaz preliminar

--- app.vue
+++ app.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline text-yellow-500">
-      Hello world!
-    </h1>
+    <!-- Incluir el componente CarControl -->
+    <CarControl />
   </div>
 </template>
+
+<script>
+// Importar el componente
+import CarControl from '~/components/carControl.vue';
+
+export default {
+  components: {
+    CarControl
+  }
+};
+</script>
